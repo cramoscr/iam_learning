cb5f1f4b0644ec7ba2d2ff538a121e5963a9000e
carlos.ramos <cramoscr@gmail.com>
2023-01-02 12:41:15 -0600

    Intentionally making mistakes

--- python/elemental_scripts/25_try_catch.py
+++ python/elemental_scripts/25_try_catch.py
@@ -4,6 +4,23 @@
 #
 # This sample shows how to handle errors in Python
 
+basura
+asdfasdf
+asdf
+asdf
+asdf
+asdf
+12312
+12
+1
+31
+3asd1f
+a3d1f
+as1df
+3a1sdf
+a1sdf
+a31dsf
+
 while True:
     a = input('\n Enter a number value ["q" for exit]: ')
 
