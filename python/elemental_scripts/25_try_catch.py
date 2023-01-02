9e43c24d1fd63523a5a2fd30a8f7d06163112c89
carlos.ramos <cramoscr@gmail.com>
2023-01-02 12:37:54 -0600

    More improvements on python files

--- python/elemental_scripts/25_try_catch.py
+++ python/elemental_scripts/25_try_catch.py
@@ -1,7 +1,8 @@
 # 25_try_catch.py 
 # ------------
+# Updated: cramos 02/jan/2022
+#
 # This sample shows how to handle errors in Python
-
 
 while True:
     a = input('\n Enter a number value ["q" for exit]: ')
